d1c67a4fa9e7f7e66dd2e9f6266771d39a17d571
helmikhiari <helmikhiari@users.noreply.github.com>
2024-11-06 08:12:28 +0100

    firsttt

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
     entry: './server.js',  // Update the entry point to your main file
     output: {
         path: path.resolve(__dirname, 'dist'),  // Output directory
-        filename: 'main.js',                    // Output filename
+        filename: 'main.js',                       // Output filename
     },
     resolve: {
         extensions: ['.js', '.json'],           // Extensions to resolve
